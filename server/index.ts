f190266db42b21d155d369e0134b89d71d73220b
lydialydd <lydialyd@live.unc.edu>
2019-06-03 14:36:41 +0200

    tivoli

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,9 @@
-import * as express from "express";
-import * as exphbs from "express-handlebars";
 import * as dotenv from "dotenv";
 dotenv.config();
 
+import * as express from "express";
+import * as exphbs from "express-handlebars";
+import { DB, Rows } from "./db";
 
 let app = express();
 app.use(express.static("dist/"));
@@ -21,6 +22,26 @@ app.get("/", (req, res) => {
 app.get("/index", (req, res) => {
     res.render("index", {title: "Home"});
 });
+
+
+app.get("/todos.json", async (req, res) => {
+    let [rows, fields] = await DB.query<Rows>("SELECT * FROM todos");
+    res.json(rows);
+});
+
+app.get("/todos", async (req, res) => {
+    let [rows] = await DB.query<Rows>("SELECT * FROM todos");
+    res.render("todos-demo", {todos: rows});
+    
+});
+
+
+// how you can click on diff blog posts in diff pages when added to database
+app.get("/todos/:id", async (req, res) => {
+    let [rows] = await DB.query<Rows>("SELECT * FROM todos WHERE id = :id", {id: req.params.id});
+    res.json(rows);
+});
+
 
 app.get("/gallery", (req, res) => {
     res.render("gallery", {title: "Photos"});
